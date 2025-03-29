4292f76545198dc4e1e6a36a20f2a5e37de6e507
AJprSINGH <ajit.12008255@lpu.in>
2025-03-29 21:06:25 +0530

    103

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,14 @@ import { authMiddleware } from "@clerk/nextjs";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["/login"], // Allow users to access "/login"
+  afterAuth(auth, req) {
+    if (!auth.userId && req.nextUrl.pathname !== "/login") {
+      return Response.redirect(new URL("/login", req.url)); // Redirects to "/login"
+    }
+  },
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
