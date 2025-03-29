0406ccec70bf2be67106a0b4e64680b5fd7c36d6
AJprSINGH <ajit.12008255@lpu.in>
2025-03-29 21:09:58 +0530

    104

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/login"], // Allow users to access "/login"
-  afterAuth(auth, req) {
-    if (!auth.userId && req.nextUrl.pathname !== "/login") {
-      return Response.redirect(new URL("/login", req.url)); // Redirects to "/login"
-    }
-  },
+  publicRoutes: ["/"],
 });
 
 export const config = {
